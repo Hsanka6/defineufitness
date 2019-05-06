97ef3803e5521f5e63c2161b57abe744c5b2cc63
Hsanka6 <haasith2@gmail.com>
2019-05-06 16:52:11 -0700

    Made navbar and auth into component.js files

---
+++ src/components/Auth.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const Auth = () => {
+    return (
+    <div className="columns is-centered">
+        {/*<div className="column is-half has-background-white">*/}
+            {/*<div className="field">*/}
+                {/*<label className="label">Username</label>*/}
+                {/*<div className="control has-icons-left has-icons-right">*/}
+                    {/*<input className="input " type="email" placeholder="Email"/>*/}
+                {/*<span className="icon is-small is-left">*/}
+                  {/*<i className="fas fa-user"/>*/}
+                {/*</span>*/}
+
+                {/*</div>*/}
+            {/*</div>*/}
+            {/*<div className="field">*/}
+                {/*<label className="label">Password</label>*/}
+                {/*<div className="control has-icons-left has-icons-right">*/}
+                    {/*<input className="input " type="password" placeholder="Password"/>*/}
+                {/*<span className="icon is-small is-left">*/}
+                  {/*<i className="fas fa-lock"/>*/}
+                {/*</span>*/}
+
+                {/*</div>*/}
+            {/*</div>*/}
+
+
+        {/*</div>*/}
+        <div className="column is-half">
+        <div className="section">
+            <div className="field">
+                <label className="label">Username</label>
+                <div className="control has-icons-left has-icons-right">
+                    <input className="input " type="email" placeholder="Email"/>
+                    <span className="icon is-small is-left">
+                  <i className="fas fa-user"/>
+                </span>
+
+                </div>
+            </div>
+            <div className="field">
+                <label className="label">Password</label>
+                <div className="control has-icons-left has-icons-right">
+                    <input className="input " type="password" placeholder="Password"/>
+                    <span className="icon is-small is-left">
+                  <i className="fas fa-lock"/>
+                </span>
+
+                </div>
+            </div>
+            <button className="button is-primary has-text-centered">Center me</button>
+        </div>
+        </div>
+
+    </div>
+
+
+
+
+    )
+}
+
+export default Auth
---
+++ src/components/SignedInTabs.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import navbar from './components/SignedInTabs.js';
+//
+// function SignedInTabs() {
+//     return (
+//         <div className="navbar-brand">
+//             <a className="navbar-item" href="https://bulma.io">
+//                 <img src="https://bulma.io/images/bulma-logo.png" width="112" height="50"/>
+//             </a>
+//
+//             <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
+//                data-target="navbarBasicExample">
+//                 <span aria-hidden="true"></span>
+//                 <span aria-hidden="true"></span>
+//                 <span aria-hidden="true"></span>
+//             </a>
+//         </div>
+//         <div id="navbarBasicExample" className="navbar-menu">
+//             <div className="navbar-start is-size-3">
+//             <a className="navbar-item">
+//             Clients
+//             </a>
+//
+//         <a className="navbar-item">
+//             Events
+//         </a>
+//
+//
+//         </div>
+//
+//         <div className="navbar-end">
+//             <div className="navbar-item is-size-3">
+//
+//                 Login
+//
+//             </div>
+//         </div>
+//         </div>
+//
+//     );
+// }
+//
+// export default SignedInTabs;
